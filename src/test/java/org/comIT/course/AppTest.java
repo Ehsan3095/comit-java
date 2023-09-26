ca7060bcef5064ff430dc77e0b06bf8e206835c0
KouroshKaviany <96090662+KouroshKaviany@users.noreply.github.com>
2023-09-25 21:20:20 -0300

    first version

---
+++ src/test/java/org/comIT/course/AppTest.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize two int variables
+        int num1 = 23452;
+        int num2 = 45363;
+        
+        // Print the variables to the console
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+    }
+}
